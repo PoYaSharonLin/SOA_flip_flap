9c1ced80c6cc2f4c5dff16e3a35ce0283823c4e9
poyasharonlin <sharon.lin@iss.nthu.edu.tw>
2025-09-25 16:40:27 +0800

    fix: use mixin to fix undefined method 'take_tsv' for an instance of FlipFlap (NoMethodError)

--- flip_flap.rb
+++ flip_flap.rb
@@ -6,6 +6,7 @@ require_relative 'yaml_buddy'
 # Converts tabular data between storage formats
 class FlipFlap
   # Do NOT create an initialize method
+  include TsvBuddy
   attr_reader :data
 
   def self.input_formats
