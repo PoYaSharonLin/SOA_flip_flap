3b83dd49b0d96932a3069888246f25015301c21e
poyasharonlin <sharon.lin@iss.nthu.edu.tw>
2025-09-25 16:47:04 +0800

    fix: created YamlBuddy; use mixin to fix undefined method 'take_yaml' for an instance of FlipFlap (NoMethodError)

--- flip_flap.rb
+++ flip_flap.rb
@@ -7,6 +7,7 @@ require_relative 'yaml_buddy'
 class FlipFlap
   # Do NOT create an initialize method
   include TsvBuddy
+  include YamlBuddy
   attr_reader :data
 
   def self.input_formats
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 # module YamlBuddy that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  attr_accessor :data
+
+  # Converts a YAML string into @data, an array of hashes 
+  def take_yaml(yml)
+  end 
+
+  # Converts @data back to yaml 
+  def to_yaml
+  end 
+end 
+
 
