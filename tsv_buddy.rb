cbf756691ce1ca4149936b6537313ee09c44744a
poyasharonlin <sharon.lin@iss.nthu.edu.tw>
2025-09-25 22:04:10 +0800

    refactor: refactor the code to match the gist

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -6,11 +6,19 @@ module TsvBuddy
 
   # Converts a TSV string into @data, an array of hashes
   def take_tsv(tsv)
-
+    lines = tsv.split("\n")
+    headers = lines[0].split("\t")
+    @data = lines[1..].map { |line| headers.zip(line.split("\t")).to_h }
   end
+  # reference: https://gist.github.com/PoYaSharonLin/16bbcc18a244e7d647477489febc92fd
 
   # Converts @data back to a TSV string
   def to_tsv
-
+    headers = @data.first.keys
+    rows = @data.map do |hash|
+      headers.map { |key| hash[key].to_s }.join("\t")
+    end
+    "#{[headers.join("\t"), *rows].join("\n")}\n"
   end
+  # reference: https://gist.github.com/PoYaSharonLin/86bfe9b26cfae25fda7291db2588b779
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
+require 'yaml'
 # module YamlBuddy that can be included (mixin) to take and output Yaml data
 module YamlBuddy
   attr_accessor :data
 
-  # Converts a YAML string into @data, an array of hashes 
+  # Converts a YAML string into @data, an array of hashes
   def take_yaml(yml)
-  end 
+    @data = YAML.safe_load(yml)
+  end
 
-  # Converts @data back to yaml 
+  # Converts @data back to yaml
   def to_yaml
-  end 
-end 
-
-
+    @data.to_yaml
+  end
+end
